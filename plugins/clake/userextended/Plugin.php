49e437f1edfa4f8e357e3902f991d8f8a9c4fc83
ShawnClake <shawn.clake@gmail.com>
2016-10-13 22:14:50 -0600

    Initial Commit :)

---
+++ plugins/clake/userextended/Plugin.php
@@ -0,0 +1,99 @@
+<?php namespace Clake\UserExtended;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * UserExtended Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+//test
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'UserExtended',
+            'description' => 'No description provided yet...',
+            'author'      => 'clake',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        //return []; // Remove this line to activate
+
+        return [
+            'Clake\UserExtended\Components\UserGroups' => 'usergroups',
+            'Clake\UserExtended\Components\ListFriends' => 'friends',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'clake.userextended.some_permission' => [
+                'tab' => 'UserExtended',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'userextended' => [
+                'label'       => 'UserExtended',
+                'url'         => Backend::url('clake/userextended/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['clake.userextended.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+
+}
