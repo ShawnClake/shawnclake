c8a1321d6aa472535f7848aa4fbe10d1830656fd
ShawnClake <shawn.clake@gmail.com>
2016-10-17 17:20:40 -0600

    Fixing a bug that caused this migration to fail

--- plugins/clake/dungeons/updates/add_unique_to_dungeon_slug.php
+++ plugins/clake/dungeons/updates/add_unique_to_dungeon_slug.php
@@ -5,14 +5,13 @@ use October\Rain\Database\Schema\Blueprint;
 use Schema;
 use October\Rain\Database\Updates\Migration;
 
-class UsersAddLastSeen extends Migration
+class AddUniqueToDungeonSlug extends Migration
 {
     public function up()
     {
         Schema::table('clake_dungeons_dungeons', function(Blueprint $table)
         {
-            $table->dropColumn('slug');
-            $table->string('slug')->unique();
+            $table->string('slug')->unique()->change();;
         });
     }
 
@@ -20,8 +19,7 @@ class UsersAddLastSeen extends Migration
     {
         Schema::table('clake_dungeons_dungeons', function($table)
         {
-            $table->dropColumn('slug');
-            $table->string("slug");
+            $table->string("slug")->change();;
         });
     }
 }
