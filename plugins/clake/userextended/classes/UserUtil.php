426dea9c347f88c5570e4d48fb4fe048b4c1fc51
Shawn Clake <shawn.clake@gmail.com>
2016-11-25 16:44:53 -0600

    Added documentation and timezone methods. As well, added model casts

--- plugins/clake/userextended/classes/UserUtil.php
+++ plugins/clake/userextended/classes/UserUtil.php
@@ -3,6 +3,8 @@
 namespace Clake\UserExtended\Classes;
 
 use Auth;
+use Carbon\Carbon;
+use Clake\Userextended\Models\Timezone;
 use Clake\Userextended\Models\UserExtended;
 use RainLab\User\Models\User;
 use Redirect;
@@ -10,6 +12,10 @@ use Redirect;
 /**
  * Class UserUtil
  * @package Clake\UserExtended\Classes
+ *
+ * @todo: Move time related methods to a seperate trait
+ * @todo: Test casting and timezones
+ *
  */
 class UserUtil
 {
@@ -73,4 +79,147 @@ class UserUtil
         if(!self::getLoggedInUser())
             return Redirect::to($url);
     }
+
+    /**
+     * Returns a Timezone model for the current logged in user
+     * @return mixed|null|string
+     */
+    public static function getLoggedInUsersTimezone()
+    {
+        $user = self::getLoggedInUser();
+        $user = self::castToUserExtendedUser($user);
+        if($user != null)
+            return $user->timezone;
+        return null;
+    }
+
+    /**
+     * Get a users current timezone.
+     * @param $value
+     * @param string $property
+     * @return null
+     */
+    public static function getUserTimezone($value, $property = "id")
+    {
+        $user = self::getUser($value, $property);
+        if($user != null)
+            return $user->timezone;
+        return null;
+    }
+
+    /**
+     * Adjust the current UTC time by minutes, hours, days, and seconds
+     * @param $minutes
+     * @param int $hours
+     * @param int $days
+     * @param int $seconds
+     * @return Carbon
+     */
+    public static function getCurrentTimeAdjusted($minutes, $hours = 0, $days = 0, $seconds = 0)
+    {
+        $current = Carbon::now();
+        $current->addHours($hours);
+        $current->addMinutes($minutes);
+        $current->addDays($days);
+        $current->addSeconds($seconds);
+        return $current;
+    }
+
+    /**
+     * Take a time string such as 2:30 or 4:00 and convert it into minutes and hours
+     * @param $time
+     * @return array
+     */
+    private static function getTimeStringAdjustments($time)
+    {
+        $time = explode(":", $time);
+        $minutes = 0;
+        $hours = $time[0];
+        if(isset($time[1]))
+            $minutes = $time[1];
+        return [
+            'minutes' => $minutes,
+            'hours' => $hours,
+        ];
+    }
+
+    /**
+     * Get the current time adjusted via the logged in Users timezone
+     * @return Carbon
+     */
+    public static function getLoggedInUsersCurrentTimeAdjusted()
+    {
+        $offset = self::getLoggedInUsersTimezone()->offset;
+        $adjustment = self::getTimeStringAdjustments($offset);
+        return self::getCurrentTimeAdjusted($adjustment['minutes'], $adjustment['hours']);
+    }
+
+    /**
+     * Gets the current time adjusted via a Users timezone
+     * @param UserExtended $user
+     * @return Carbon
+     */
+    public static function getUsersCurrentTimeAdjusted(UserExtended $user)
+    {
+        $offset = $user->timezone->offset;
+        $adjustment = self::getTimeStringAdjustments($offset);
+        return self::getCurrentTimeAdjusted($adjustment['minutes'], $adjustment['hours']);
+    }
+
+    /**
+     * Gets a time adjusted via a Timezone model.
+     * @param $time
+     * @param Timezone $timezone
+     * @return mixed
+     */
+    public static function getTimeAdjustedByTimezone($time, Timezone $timezone)
+    {
+        $offset = $timezone->offset;
+        $adjustment = self::getTimeStringAdjustments($offset);
+        return self::getTimeAdjusted($time, $adjustment['minutes'], $adjustment['hours']);
+    }
+
+    /**
+     * Adjusts an arbitrary time by minutes, hours, days, and seconds
+     * @param $time
+     * @param $minutes
+     * @param int $hours
+     * @param int $days
+     * @param int $seconds
+     * @return mixed
+     */
+    public static function getTimeAdjusted($time, $minutes, $hours = 0, $days = 0, $seconds = 0)
+    {
+        $time->addHours($hours);
+        $time->addMinutes($minutes);
+        $time->addDays($days);
+        $time->addSeconds($seconds);
+        return $time;
+    }
+
+    /**
+     * Casts the Rainlab.User model to Clake.UserExtended
+     * @param UserExtended $user
+     * @return User
+     */
+    public static function castToRainLabUser(UserExtended $user)
+    {
+        $rainlab = new User();
+        $rainlab->attributes = $user->attributes;
+        return $rainlab;
+    }
+
+    /**
+     * Casts the Clake.UserExtended model to Rainlab.User
+     * @param User $user
+     * @return UserExtended
+     */
+    public static function castToUserExtendedUser(User $user)
+    {
+        $userExtended = new UserExtended();
+        $userExtended->attributes = $user->attributes;
+        return $userExtended;
+    }
+
+
 }
