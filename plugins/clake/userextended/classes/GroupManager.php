882225922e990be1f68fff37060b09ad031d79fa
Shawn Clake <shawn.clake@gmail.com>
2016-11-26 16:43:13 -0600

    Added documentation and a few misc functions

--- plugins/clake/userextended/classes/GroupManager.php
+++ plugins/clake/userextended/classes/GroupManager.php
@@ -12,11 +12,27 @@ namespace Clake\UserExtended\Classes;
 use Clake\Userextended\Models\GroupsExtended;
 use October\Rain\Support\Collection;
 
+/**
+ * Class GroupManager
+ * @package Clake\UserExtended\Classes
+ *
+ * Handles all interaction accross groups on a global level rather than a user level.
+ */
 class GroupManager
 {
 
+    // A collection of groups
     private $groups;
 
+    public static function retrieve($code)
+    {
+        return GroupsExtended::where('code', $code)->first();
+    }
+
+    /**
+     * Creates and fills the class with all of the groups that exist in the applciation
+     * @return static
+     */
     public static function all()
     {
         $instance = new static;
@@ -33,14 +49,32 @@ class GroupManager
         return $instance;
     }
 
+    /**
+     * Returns a count of how many groups there are
+     * @return mixed
+     */
     public function count()
     {
         return $this->groups->count();
     }
 
+    /**
+     * Returns a count of how many roles there are in a specific group.
+     * @param $groupCode
+     * @return mixed
+     */
     public function roleCount($groupCode)
     {
         return $this->groups->get($groupCode)->roles()->count();
+    }
+
+    /**
+     * Returns the collection of groups.
+     * @return mixed
+     */
+    public function get()
+    {
+        return $this->groups;
     }
 
 }
