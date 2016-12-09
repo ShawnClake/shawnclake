44a525b36d9758ab5f8de9ad06fc8d60f6edc522
Shawn Clake <shawn.clake@gmail.com>
2016-12-09 15:09:00 -0600

    Began work on a better logging in component

---
+++ plugins/clake/userextended/components/Login.php
@@ -0,0 +1,21 @@
+<?php namespace Clake\Userextended\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Login extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Login Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+
+}
