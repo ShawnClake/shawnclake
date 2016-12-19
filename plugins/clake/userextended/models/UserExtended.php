5a927e6b443288c02800014630901873dc0df823
Shawn Clake <shawn.clake@gmail.com>
2016-12-19 14:39:27 -0600

    Added timezonable and searchable traits

--- plugins/clake/userextended/models/UserExtended.php
+++ plugins/clake/userextended/models/UserExtended.php
@@ -4,18 +4,36 @@ use Model;
 use RainLab\User\Models\User;
 
 use Clake\UserExtended\Traits\Timezonable;
+use Clake\UserExtended\Traits\Searchable;
 
 /**
- * UserExtended Model
+ * TODO: Add searchable functions here
+ * TODO: Add searchable trait
+ * TODO: Add scope functions for better user querying
+ */
+
+/**
+ * Class UserExtended
+ * @package Clake\Userextended\Models
  */
 class UserExtended extends User
 {
 
-    protected $timezonable = [
+    use Timezonable;
 
+    use Searchable;
+
+
+    protected $timezonable = [
         'created_at',
         'updated_at'
+    ];
 
+    protected $searchable = [
+        'email',
+        'name',
+        'surname',
+        'username'
     ];
 
     /**
