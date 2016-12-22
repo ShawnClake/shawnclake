0e4397bf240497f5fc0fa72127dbf9c65229ac93
Shawn Clake <shawn.clake@gmail.com>
2016-12-22 14:03:47 -0600

    Added a register component.... Currently just a placeholder.

---
+++ plugins/clake/userextended/components/Register.php
@@ -0,0 +1,21 @@
+<?php namespace Clake\UserExtended\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Register extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Register Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+
+}
